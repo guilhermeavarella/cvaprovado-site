a4bc9d9a7575395f729bc4ebe0a8b5dba4fac5a2
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-12-13 13:31:23 -0300

    feat: style settings and variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <section className="min-w-screen min-h-screen flex flex-col items-center justify-start">
       <div>
         Screen
+        <p>This is a paragraph to test the font family.</p>
       </div>
     </section>
   )
