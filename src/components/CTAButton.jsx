a11fb0b3e8858ce30ecc765ffbeacdfe928b6a78
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-12-14 14:58:05 -0300

    feat: CTA button features

--- src/components/CTAButton.jsx
+++ src/components/CTAButton.jsx
@@ -6,12 +6,18 @@ import blackArrow from "../assets/icons/button-arrow-black.svg";
 export default function Button({ label, variant }) {
     return (
         variant === "white" ?
-        <button className="flex flex-row items-center bg-[var(--background-fixed-white)] text-[var(--content-primary)] px-8 py-3.5 gap-4 rounded-xl hover:opacity-80 transition-opacity">
+        <button 
+        className="flex flex-row items-center bg-[var(--background-fixed-white)] text-[var(--content-primary)] px-8 py-3.5 gap-4 rounded-xl hover:opacity-80 hover:cursor-pointer transition-opacity"
+        onClick={() => { window.open('https://wa.me/5511963991859'); }}
+        >
             <p className="font-semibold font-Geologica">{label}</p>
             <img src={blackArrow} className="w-4 h-4"/>
         </button>
         :
-        <button className="flex flex-row items-center bg-[var(--brand-secondary)] text-[var(--content-inverse)] px-8 py-3.5 gap-4 rounded-xl hover:opacity-80 transition-opacity">
+        <button 
+        className="flex flex-row items-center bg-[var(--brand-secondary)] text-[var(--content-inverse)] px-8 py-3.5 gap-4 rounded-xl hover:opacity-80 hover:cursor-pointer transition-opacity"
+        onClick={() => { window.open('https://wa.me/5511963991859'); }}
+        >
             <p className="font-semibold font-Geologica">{label}</p>
             <img src={whiteArrow} className="w-4 h-4"/>
         </button>
