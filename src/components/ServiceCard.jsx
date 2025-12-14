cd1ac7af1583cd65a0a84fbdc22be2c0cc629aec
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-12-14 19:40:40 -0300

    feat: card component for services

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,9 @@
+export default function ServiceCard({ title, description, icon }) {
+    return (
+        <div className="w-full md:w-[17.25rem] h-[18rem] md:h-[21.5rem] flex flex-col items-center bg-[var(--background-fixed-white)] rounded-2xl shadow-sm px-4 py-8 gap-4">
+            <img src={`/icons/services/${icon}.svg`} className="w-16 h-16"/>
+            <h5 className="text-center">{title}</h5>
+            <p className="text-center">{description}</p>
+        </div>
+    )
+}
