6356518eda93032a7944c29ad1e2221dad8961d4
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-12-14 16:29:51 -0300

    style: minor style fixes

--- src/components/CTAButton.jsx
+++ src/components/CTAButton.jsx
@@ -7,7 +7,7 @@ export default function Button({ label, variant }) {
     return (
         variant === "white" ?
         <button 
-        className="flex flex-row items-center bg-[var(--background-fixed-white)] text-[var(--content-primary)] px-8 py-3.5 gap-4 rounded-xl hover:opacity-80 hover:cursor-pointer transition-opacity"
+        className="flex flex-row items-center bg-[var(--background-fixed-white)] text-[var(--content-primary)] px-8 py-4 gap-4 rounded-xl hover:opacity-80 hover:cursor-pointer transition-opacity"
         onClick={() => { window.open('https://wa.me/5511963991859'); }}
         >
             <p className="font-semibold font-Geologica">{label}</p>
@@ -15,7 +15,7 @@ export default function Button({ label, variant }) {
         </button>
         :
         <button 
-        className="flex flex-row items-center bg-[var(--brand-secondary)] text-[var(--content-inverse)] px-8 py-3.5 gap-4 rounded-xl hover:opacity-80 hover:cursor-pointer transition-opacity"
+        className="flex flex-row items-center bg-[var(--brand-secondary)] text-[var(--content-inverse)] px-8 py-4 gap-4 rounded-xl hover:opacity-80 hover:cursor-pointer transition-opacity"
         onClick={() => { window.open('https://wa.me/5511963991859'); }}
         >
             <p className="font-semibold font-Geologica">{label}</p>
