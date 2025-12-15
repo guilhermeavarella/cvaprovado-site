f51fc6073dddb8e27d0f3faf2344299825bd6705
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-12-15 13:36:39 -0300

    fix: minor style fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="w-screen bg-[var(--background-default)] flex flex-col items-center justify-start">
       {/* Background Image and Hero*/}
-      <div className="absolute top-0 w-full h-[54rem] flex flex-col items-center overflow-visible">
+      <div className="absolute top-0 w-screen h-[54rem] flex flex-col items-center overflow-visible">
         <div className="w-full h-[54rem] bg-[var(--background-inverse)]"/>
         <img src={heroImage} className="w-screen mt-[-1.25rem] lg:mt-[-5.5rem]"/>
       </div>
