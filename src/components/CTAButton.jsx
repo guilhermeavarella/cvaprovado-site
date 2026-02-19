81920fe97e401ddaca53427cfa00297f0d5f04c9
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2026-02-19 11:37:23 -0300

    feat: button animation update

--- src/components/CTAButton.jsx
+++ src/components/CTAButton.jsx
@@ -10,7 +10,7 @@ export default function Button({ label, variant }) {
         <motion.button 
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
-        className="flex flex-row items-center bg-[var(--background-fixed-white)] text-[var(--content-primary)] px-8 py-4 gap-4 rounded-xl hover:opacity-80 hover:cursor-pointer transition-opacity"
+        className="flex flex-row items-center bg-[var(--background-fixed-white)] text-[var(--content-primary)] px-8 py-4 gap-4 rounded-xl hover:brightness-95 hover:cursor-pointer transition-opacity"
         onClick={() => { window.open('https://wa.me/5511963991859'); }}
         >
             <p className="font-semibold font-Geologica">{label}</p>
@@ -20,7 +20,7 @@ export default function Button({ label, variant }) {
         <motion.button
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.95 }}
-        className="flex flex-row items-center bg-[var(--brand-secondary)] text-[var(--content-inverse)] px-8 py-4 gap-4 rounded-xl hover:opacity-80 hover:cursor-pointer transition-opacity"
+        className="flex flex-row items-center bg-[var(--brand-secondary)] text-[var(--content-inverse)] px-8 py-4 gap-4 rounded-xl hover:brightness-95 hover:cursor-pointer transition-opacity"
         onClick={() => { window.open('https://wa.me/5511963991859'); }}
         >
             <p className="font-semibold font-Geologica">{label}</p>
